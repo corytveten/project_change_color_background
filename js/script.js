be67f714152f59a49816796673f785ab045c576b
corytveten <corytveten@gmail.com>
2020-06-26 19:42:45 -0700

    color randomizer

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@
 const button = document.getElementsByClassName('btn btn-outline-secondary')
 
 function changeColor() {
-    document.body.style.backgroundColor = 'dodgerBlue';
+    document.body.style.backgroundColor = random_bg_color();
 }
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
 
 button[0].addEventListener('click', changeColor)
